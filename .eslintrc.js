9946158dbf9bd4efdc7a9cb6b7de165a40c6843c
Samuel Mena <samuel23mena@gmail.com>
2024-06-01 20:57:14 -0600

    Adding screens

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+        bracketSpacing: 1,
+        singleQuote: 1,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-native/no-inline-styles': 0,
+  },
 };
