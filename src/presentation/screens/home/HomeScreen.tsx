40dcac2eb425127f9cb2f704a33b0fd53a0bddbe
Samuel Mena <samuel23mena@gmail.com>
2024-06-03 22:05:39 -0600

    :sparkles: Adding Light & Dark theme

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,14 @@
-import {View, Text} from 'react-native';
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text variant="displaySmall">HomeScreen</Text>
+
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   );
 };
